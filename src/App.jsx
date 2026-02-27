19e8815e20cd2492e8788a4e45c330f45585af62
Praful kumar <96467257+prafulk9155@users.noreply.github.com>
2026-02-27 18:05:18 +0000

    added for variable asn function in jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
-// export fedault us directly used while {} use to export only 
-import UserProfile, { ThanksMsg } from "./UserProfile";
+// import MyFirstComponent from "./MyFirstComponent";
+// export fedault us directly used while {} use to export only
+// import UserProfile, { ThanksMsg } from "./UserProfile";
+
+import JSXExample from "./JSXExample";
+
 export default function App() {
   return (
     <>
       <h1>Main App</h1>
-      <UserProfile />
-      <ThanksMsg />
+      {/* <MyFirstComponent /> */}
+      {/* <UserProfile />
+      <ThanksMsg /> */}
+      <JSXExample />
     </>
   );
 }
---
+++ src/JSXExample.jsx
@@ -0,0 +1,22 @@
+export default function JSXExample() {
+  const name = "XYZ";
+  const age = 25;
+  function callAlert() {
+    alert("alert called");
+  }
+  function sum(a, b) {
+    return a + b;
+  }
+
+  return (
+    <>
+      <h2>This is user section</h2>
+      {/* curl bracket to get variablr value */}
+      <p>Name: {name}</p>
+      <p>Age: {age}</p>
+      <p>Sum value : {sum(5, 6)}</p>
+      {/* onClick with function name to call function  we dont add () to the function . if we add () to function name it directly called the function*/}
+      <button onClick={callAlert}> Show Alert</button>
+    </>
+  );
+}
